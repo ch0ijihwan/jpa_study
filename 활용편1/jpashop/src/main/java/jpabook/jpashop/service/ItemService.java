91b64b58cf9f412fcc9fb42c220b07ef4f7a2a91
cjh8746 <cjh87467@gmail.com>
2022-09-13 16:51:17 +0900

    회원 등록 API

--- 활용편1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 활용편1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,7 @@ public class ItemService {
     }
 
     @Transactional
-    public void updateItem(Long itemId, String name , int price, int stockQuantity) {
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
         Item findItem = itemRepository.findOne(itemId);
         findItem.setPrice(price);
         findItem.setName(name);
