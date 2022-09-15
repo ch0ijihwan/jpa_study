2f15d2d324d3707e7d362f1b0e864ec2f1c5c31b
cjh8746 <cjh87467@gmail.com>
2022-09-15 16:57:54 +0900

    주문 조회 V3.1 : 엔티티를 DTO로 변환 - 페이징과 한계 돌파

---
+++ 활용편1/jpashop/src/main/java/jpabook/jpashop/repository/Order/simplequery/OrderSimpleQueryDto.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.repository.Order.simplequery;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime localDateTime;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.localDateTime = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
