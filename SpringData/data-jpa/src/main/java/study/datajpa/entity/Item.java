55828489f724da3df7dbf57972cefa518e975a2e
cjh8746 <cjh87467@gmail.com>
2022-09-24 20:27:36 +0900

    새로운 엔티티를 구별하는 방법 - isNew 재정의

---
+++ SpringData/data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,35 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class Item implements Persistable<String> {
+
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    protected Item() {
+    }
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+}
